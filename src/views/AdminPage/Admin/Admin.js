0f6389b642840d178e42552170e1ef3b2e05f894
m4mohammadkhalid <61066087+m4mohammadkhalid@users.noreply.github.com>
2021-03-14 14:23:19 +0530

    save

--- src/views/AdminPage/Admin/Admin.js
+++ src/views/AdminPage/Admin/Admin.js
@@ -10,10 +10,10 @@ const Admin = () => {
             <div className="container">
                 <div className="header-text">
                     <img className="header-text-logo" src="https://seeklogo.com/images/E/educational-people-logo-66BC945BD1-seeklogo.com.png" alt="logo" />
-                    <p className="header-text-description">Student Login</p>
+                    <p className="header-text-description">Student Login Khalid</p>
                 </div>
                 <form>
-                    <input type="text" placeholder="Email" name="email" id="email" autofocus />
+                    <input type="text" placeholder="Email" name="email" id="email" autoFocus />
                     <input type="password" name="password" id="password" placeholder="Password" required />
                     <button type="submit" name="login">Login</button>
                 </form>
