bc30ba475d2db72079a5e5741c7291075541d79c
m4mohammadkhalid <61066087+m4mohammadkhalid@users.noreply.github.com>
2021-03-22 17:36:42 +0530

    upload

--- src/views/HomeMainContent/HomePage/PopularCourses.js
+++ src/views/HomeMainContent/HomePage/PopularCourses.js
@@ -1,16 +1,16 @@
-import React,{useState} from "react";
-import { Link,withRouter } from "react-router-dom";
-import { listBlogsWithCategoriesAndTags } from '../../../components/Middleware/blog';
+import React from "react";
+import { Link } from "react-router-dom";
+//import { listBlogsWithCategoriesAndTags } from '../../../components/Middleware/blog';
 
 const PopularCourses = () => {
-  const [data, setData] = useState()
-  const apiget = () => {
-    fetch('https://adminsparrow.herokuapp.com/blogs')
-  .then(response => response.json())
-      .then((json) => {
-    setData(json)
-  });
-  }
+  // const [data, setData] = useState()
+  // const apiget = () => {
+  //   fetch('https://adminsparrow.herokuapp.com/blogs')
+  // .then(response => response.json())
+  //     .then((json) => {
+  //   setData(json)
+  // });
+  // }
   return (
     <section>
       <div className="container">
@@ -22,8 +22,8 @@ const PopularCourses = () => {
               data-animation-delay="0.01s"
             >
               <div className="heading_s1 text-center">
-                <button onClick={apiget}>Button</button>
-                <h2>{JSON.stringify(data)} Courses</h2>
+                {/* <button onClick={apiget}>Button</button> */}
+                {/* <h2>{JSON.stringify(data)} Courses</h2> */}
               </div>
               <p>
                 If you are going to use a passage of Lorem Ipsum, you need to be
