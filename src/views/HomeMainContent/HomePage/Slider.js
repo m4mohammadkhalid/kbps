a238e04b2a08cdd290bb1886ac5ae9087efa33a2
m4mohammadkhalid <61066087+m4mohammadkhalid@users.noreply.github.com>
2021-03-28 04:34:44 +0530

    updates slider

--- src/views/HomeMainContent/HomePage/Slider.js
+++ src/views/HomeMainContent/HomePage/Slider.js
@@ -1,111 +1,12 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
+import CarouselContainer from '../../../components/CarouselContainer';
 
 const Slider = () => {
-  
+
   return (
-    
-    <>
-      
 
-  <div className="swiper-container main-slider loading">
-    <div className="swiper-wrapper">
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLbVhsNzdIYmlfN1E)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLbVhsNzdIYmlfN1E" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Shaun Matthews</p>
-          <span className="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLWTdaX3J5b1VueDg)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLWTdaX3J5b1VueDg" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Alexis Berry</p>
-          <span className="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLRml1b3B6eXVqQ2s)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLRml1b3B6eXVqQ2s" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Billie	Pierce</p>
-          <span className="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLVUpEems2ZXpHYVk)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLVUpEems2ZXpHYVk" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Trevor	Copeland</p>
-          <span className="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLNXBIcEdOUFVIWmM)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLNXBIcEdOUFVIWmM" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Bernadette	Newman</p>
-          <span className="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-        </div>
-      </div>
-    </div>
-    <div className="swiper-button-prev swiper-button-white" />
-    <div className="swiper-button-next swiper-button-white" />
-  </div>
-  <div className="swiper-container nav-slider loading">
-    <div className="swiper-wrapper" role="navigation">
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLbVhsNzdIYmlfN1E)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLbVhsNzdIYmlfN1E" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Shaun Matthews</p>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLWTdaX3J5b1VueDg)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLWTdaX3J5b1VueDg" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Alexis Berry</p>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLRml1b3B6eXVqQ2s)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLRml1b3B6eXVqQ2s" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Billie	Pierce</p>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLVUpEems2ZXpHYVk)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLVUpEems2ZXpHYVk" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Trevor	Copeland</p>
-        </div>
-      </div>
-      <div className="swiper-slide">
-        <figure className="slide-bgimg" style={{backgroundImage: 'url(https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLNXBIcEdOUFVIWmM)'}}>
-          <img src="https://drive.google.com/uc?export=view&id=0B_koKn2rKOkLNXBIcEdOUFVIWmM" className="entity-img" alt="khalid" />
-        </figure>
-        <div className="content">
-          <p className="title">Bernadette	Newman</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-
-      </>
+    <CarouselContainer />
   )
 }
 
