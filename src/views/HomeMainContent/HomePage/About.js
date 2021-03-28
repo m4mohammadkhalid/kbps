c8204487133c9bddcd4c7dc597f3db2a48f8084c
m4mohammadkhalid <61066087+m4mohammadkhalid@users.noreply.github.com>
2021-03-28 23:32:02 +0530

    updates

--- src/views/HomeMainContent/HomePage/About.js
+++ src/views/HomeMainContent/HomePage/About.js
@@ -2,33 +2,36 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 const About = () => {
-    return (
-        <>
-<section className="overflow_hide">
-  <div className="container">
-    <div className="row align-items-center">
-      <div className="col-md-5">
-        <div className="animation" data-animation="fadeInLeft" data-animation-delay="0.01s">
-          <img src="assets/images/about_img2.png" alt="about_img2" />
-        </div>
-      </div>
-      <div className="col-md-7">
-        <div className="padding_eight_all animation fancy_box" data-animation="fadeInRight" data-animation-delay="0.02s">
-          <div className="heading_s1"> 
-            <h2>About Us</h2>
+  return (
+    <>
+      <section className="overflow_hide">
+        <div className="container">
+        <div className="heading_s1"> 
+            <h2><center>About Us</center></h2>
           </div>
-          <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary</p>
-          <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
-          <Link to="#!" className="btn btn-outline-black rounded-0 my-2">Read More</Link>
+          <div className="row align-items-center">
+            <div className="col-md-7">
+              <div className="animation" data-animation="fadeInLeft" data-animation-delay="0.01s">
+                <div className="floating">
+                  <div className="card-object rotate">
+                    <header></header>
+                    <aside></aside>
+                    <main><blockquote className="message">May all the <em className="forgotten">tiny</em> black <em className="forget">insects</em> crawling on the peonies be my sons <em className="forget">and daughters</em> in future lives.</blockquote></main>
+                    <footer className="author">John Giorno</footer>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5">
+              <img src="assets/images/about_img2.png" alt="about_img2" />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
+      </section>
 
 
-        </>
-    )
+    </>
+  )
 }
 
 export default About
